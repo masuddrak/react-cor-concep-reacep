22d3184e7f631db3954d4c4db36304b6ec4823ed
masud rana <masudranaabc@gmail.com>
2022-10-08 22:15:02 +0600

    added functional export import mutiple function

--- src/component/Phone/Phone.js
+++ src/component/Phone/Phone.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { add, multiply } from '../../utilitis/math';
 import PhoneDetails from '../PhoneDetails/PhoneDetails';
 
 const Phone = (props) => {
-    console.log(props)
+    const totala=add(20,30)
+    const multiplys=multiply(20,30)
     return (
         <div>
             <h2>Phone Name:{props.name}</h2>
             <PhoneDetails price={props.price}></PhoneDetails>
+            <h2>TOtal:{totala}</h2>
+            <h2>Multply:{multiplys}</h2>
         </div>
     );
 };
